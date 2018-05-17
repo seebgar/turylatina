d8f89c8a21d790d73d9cd6f9f0c2d23163e8dbe1
Sebastian Garcia <sebastiangarcia@Sebastians-MacBook-Pro-3.local>
2018-05-17 15:29:12 -0500

    cambios varios

--- turylatina/style/code.js
+++ turylatina/style/code.js
@@ -7,3 +7,36 @@ $('#rangeend').calendar({
   type: 'date',
   startCalendar: $('#rangestart')
 });
+
+
+
+
+
+$(document).ready( function() {
+    
+    $('a[href*="#"]').click(function(event) {
+        // On-page links
+        if (
+          location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+          && 
+          location.hostname == this.hostname
+                ) {
+          // Figure out element to scroll to
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+          // Does a scroll target exist?
+          if (target.length) {
+            // Only prevent default if animation is actually gonna happen
+            event.preventDefault();
+            $('html, body').animate({
+              scrollTop: target.offset().top
+            }, 1000 ); 
+              return false;
+          }
+        }
+      });
+    
+    
+    
+} );
+
